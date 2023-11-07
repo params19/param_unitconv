583a89fac25db3f271910c89c7816d91c1a33605
params19 <paramsrivastava5@gmail.com>
2023-11-06 20:54:08 -0800

    updated with first page

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "units_conv"
+include(":app")
+ 
